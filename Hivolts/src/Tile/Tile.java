3ce7083ffe18918db69fa95833b7f8f27e83b4d6
Frederic Maa <fredericthins@gmail.com>
2015-09-21 11:19:48 -0700

    Add to tile

--- Hivolts/src/Tile/Tile.java
+++ Hivolts/src/Tile/Tile.java
@@ -1,8 +1,14 @@
 package Tile;
 
 public class Tile {
-	double x,y,width,height;
-	public Tile(){
+	int x,y,width,height;
+	public Tile(int x, int y, int width, int height){
 		
+	}
+	public Tile(int x,int y){
+		this.width = 64;
+		this.height = 64;
+		this.x = x;
+		this.y = y;
 	}
 }
