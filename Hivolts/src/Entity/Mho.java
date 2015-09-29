a8caf6a9170248b77b1237880f69ce198c34b3ce
EdanSneh <edan.sneh@gmail.com>
2015-09-29 10:47:38 -0700

    worked on mho movement

--- Hivolts/src/Entity/Mho.java
+++ Hivolts/src/Entity/Mho.java
@@ -4,28 +4,57 @@ public class Mho extends Entity{
 	public Mho(int x, int y){
 		super(x, y);
 	}
+	
 	void nextTurn(){
 		CalcMove(this.getX(), this.getY());
 	}
 	
 	private void CalcMove(int x, int y) {
+		int playerposx = 0;
+		int playerposy = 0;
+		boolean right;
+		boolean down;
+		
+		//check for players position in respect to the mho
 		if(playerposx == x){
 			if(playerposy > y){
 				Mcoords [1] = moveY(1);
-				break;
+				return;
 			}
 			else{
-				Mcoords [1] = moveY(-1);				
+				Mcoords [1] = moveY(-1);
+				return;
 			}
 		}
+		else if(playerposx-x > 0){
+			right = true;
+		}
+		else{
+			right = false;
+		}
 		if(playerposy == y){
-			if(playerpos)
+			if(playerposy > x){
+				Mcoords [0] = moveX(1);
+				return;
+			}
+			if(playerposy < x){
+				Mcoords [0] = moveX(-1);
+				return;
+			}
 		}
-			
+		else if(playerposy - y > 0){
+			down = true;
+		}
+		else{
+			down = true;
+		}
 		
-		if(playerposy == this.getY()){
-			moveX(1);
-		}
+		//if(getTile(x + 1,y) = Tile instanceof fence)
+		
+	
+		
+		
+		
 		
 	}
 	
